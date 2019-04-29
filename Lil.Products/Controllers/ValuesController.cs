d748cddc1e3e6e5aefbd87a418d7470f46aba754
Rodrigo Díaz Concha <rodrigo.linkedin.learning@outlook.com>
2019-04-29 17:45:05 +0200

    Initial Web API implementations

---
+++ Lil.Products/Controllers/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lil.Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        
+    }
+}
